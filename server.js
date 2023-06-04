08c8c8e53f1c09fabcf8f00e1cf2141f3ae94fba
Ali <aliehyaie9469@gmail.com>
2023-06-04 17:31:53 +0330

    finished error handling

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  process.exit(1);
+});
+
 dotenv.config();
 const DB = process.env.DATABASE_LOCAL;
 const app = require('./app');
 
 mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 }).then(con => {
-    console.log('DB connections successful!');
+  console.log('DB connections successful!');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log('app running on port 3000');
+const server = app.listen(port, () => {
+  console.log('app running on port 3000');
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name);
+  server.close(() => {
+    process.exit(1);
+  });
+  process.exit(1);
 });
